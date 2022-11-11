31030e03865fc3a8eb293dbdae4be76fc9f039f6
AlexFazio64 <alexfazio64@gmail.com>
2022-11-11 21:12:36 +0100

    VCPVIZ-25 prova link jira

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,7 @@
 import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-  // CTRL + SHIFT + I to open the developer tools
   console.log('Congratulations, "hello-extension" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The command parameter must match the command field and the onCommand field in package.json 
   let disposable = vscode.commands.registerCommand(
     "hello-extension.helloWorld",
     () => {
