cda48f7adaaddb43ac46848c7a889eda8549ca5a
simone <simonaiello2000@outlook.com>
2022-12-17 11:53:41 +0100

    non lo so

---
+++ publish_hook.pl
@@ -0,0 +1,41 @@
+#! /usr/bin/perl
+
+# Author: Fiorentino Salvatore
+# In the commit of pull request just insert: update_package_[ major | minor | patch ]
+
+$regex = 'Author:\s+(?<author_username>\w+)\s+.+\nDate:\s+(?<day>\w+)\s(?<mounth>\w+)\s(?<day_number>\w+)\s(?<hour>\d{2}):(?<minute>\d{2}):(?<second>\d{2})\s(?<year>\d{4}).+\n+(?<message>(.|\n)+?)\ndiff';
+$regex_inner_message_update_version = 'update_package_((?<major>major)|(?<minor>minor)|(?<patch>patch))';
+
+$sha = shift ;
+$output_git_log = `git show $sha` ;
+
+if($output_git_log =~ /$regex/gm){
+
+    $author_username = $+{author_username};
+    $day = $+{day};
+    $mounth = $+{mounth};
+    $day_number = $+{day_number};
+    $hour = $+{hour};
+    $minute = $+{minute};
+    $second = $+{second};
+    $year = $+{year};
+    $message = $+{message};
+    
+    if($message =~ /$regex_inner_message_update_version/gm){
+        
+        if($+{major}){
+            print("update=major\n")
+        }elsif($+{minor}){
+            print("update=minor\n")
+        }else{
+            print("update=patch\n")
+        }
+
+    }else{
+        # Default
+        print("update=patch\n")
+    }
+
+}else{
+    
+}
--- src/visualizer-integrator/visualizer.ts
+++ src/visualizer-integrator/visualizer.ts
@@ -1,67 +1,4 @@
-import { execSync } from "child_process";
-
-function main() {
-  var fs = require("fs");
-
-  var { argv } = require("yargs")
-    .scriptName("asp_visualizer")
-    .usage(
-      "Usage: node $0 -d dlv -i asp_file -t template_file -o output_directory"
-    )
-    .option("d", {
-      alias: "dlv_path",
-      describe: "The path of the dlv solver",
-      demandOption: "The dlv excutable is required.",
-      type: "string",
-    })
-    .option("i", {
-      alias: "asp_file",
-      describe: "ASP file to solve",
-      demandOption: "The input file is required.",
-      type: "string",
-    })
-    .option("o", {
-      alias: "output",
-      describe: "path to the output directory",
-      demandOption: "the output directory is required",
-      type: "string",
-    })
-    .option("t", {
-      alias: "template_file",
-      describe: "path to the template file",
-      type: "string",
-      demandOption: "The template file is required",
-    })
-    .option("n", {
-      alias: "as_number",
-      describe: "Number of AS you want to display, insert 0 for all",
-      type: "number",
-      default: 1,
-    })
-    .describe("help", "Show help.");
-  let visualizer_object = {
-    graph: `node ./asd-graph-renderer/build/script.js fromstr --template ${argv.template_file} --output ${argv.output}`,
-    tree: `node ./asd-graph-renderer/build/script.js fromstr --template ${argv.template_file} --output ${argv.output}`,
-    table: `node agile_visualizer_matrix_visualization/src/table_visualization.js fromstr --template ${argv.template_file}`,
-    matrix: `node agile_visualizer_matrix_visualization/src/matrix_visualization.js fromstr --template ${argv.template_file}`,
-  };
-
-  let rawdata = fs.readFileSync(`${argv.template_file}`);
-  let tmp = JSON.parse(rawdata);
-  let dlv_execution = `node ./NodeJs-DLV-Wrapper/dlv_wrapper.js -i ${argv.asp_file} -d ${argv.dlv_path} -n ${argv.as_number}`;
-  let dlv_result = "" + execSync(dlv_execution);
-  let res = execSync(`${visualizer_object[tmp["template"]]}`, {
-    input: `${dlv_result}\n\r`,
-  });
-
-  console.log("" + res);
-}
-
-if (require.main === module) {
-  main();
-}
-
-import { DLVWrapper, parseArgs } from "../wrapper-dlv/dlv_wrapper";
+import { run_solver } from "../wrapper-dlv/dlv_wrapper";
 // import { GraphScript } from "../visualizer-graph/src/script"; //currently doesn't export an API
 // import { MatrixCreator } from "../visualizer-matrix/src/matrix_visualization"; //currently doesn't export an API
 // import { TableCreator } from "../visualizer-matrix/src/table_visualization"; //currently doesn't export an API
@@ -73,28 +10,31 @@ export function callNode(
   as_number: number,
   out_dir: string
 ) {
-  let wrapper_args = parseArgs(dlv_path, asp_file_path, as_number);
-  let dlv_res = new DLVWrapper().execute(wrapper_args);
+  let solver_result = run_solver(dlv_path, asp_file_path, undefined, as_number);
+
+  console.log(solver_result)
 
   const visualization_type: string = "graph"; // read from template file...
 
   switch (visualization_type) {
     case "graph":
       // Call graph visualizer using module import
-      console.log(dlv_res);
       break;
 
     case "tree":
       // Call tree visualizer using module import
-      console.log(dlv_res);
       break;
 
     case "matrix":
       // Call matrix visualizer using module import
-      console.log(dlv_res);
       break;
 
     default:
-      console.log(dlv_res);
   }
 }
+
+if (require.main === module) {
+  callNode(undefined, '/home/simone/Desktop/3col_1.asp', '/home/simone/Desktop/dlv', 1, undefined)
+}
+
+
