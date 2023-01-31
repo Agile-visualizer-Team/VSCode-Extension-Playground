6674e7a5ec3fd4e05aeb3b52ec2e09a2f5f14d0b
AlexFazio64 <alexfazio64@gmail.com>
2023-01-31 18:10:21 +0100

    vsix publish fix and listener removal

--- src/extension-main/main.ts
+++ src/extension-main/main.ts
@@ -1,20 +1,17 @@
 import path = require("path");
-import process = require("process");
 import * as vscode from "vscode";
+import process = require("process");
+import { WebviewView } from "./webview";
 import { callNode } from "../visualizer-integrator/visualizer";
 import { check_folder, check_workspace, read_config } from "./check_workspace";
-import { WebviewView } from "./webview";
 
 export function activate(context: vscode.ExtensionContext) {
   if (!check_workspace()) {
     return;
   }
   check_folder();
-  console.log('Congratulations, "ASP Visualizer" is now active!');
 
-  let disposable = vscode.commands.registerCommand("asp-vis.helloWorld", () => {
-    vscode.window.showInformationMessage("Hello World");
-  });
+  console.log('Congratulations, "ASP Visualizer" is now active!');
 
   let config = vscode.commands.registerCommand("asp-vis.config", (arg) => {
     if (vscode.workspace.workspaceFolders) {
@@ -23,7 +20,6 @@ export function activate(context: vscode.ExtensionContext) {
       const data = Buffer.from(arg, "utf8");
 
       console.log(data);
-
 
       vscode.workspace.fs.writeFile(path, data).then(() => {
         vscode.window.showInformationMessage(
@@ -43,20 +39,23 @@ export function activate(context: vscode.ExtensionContext) {
     if (template === "graph") {
       config.nodes.forEach((node: any) => {
         if (!checkUniqueness(node!.atom.variables)) {
-          vscode.window.showErrorMessage("The variables of the nodes must be unique");
+          vscode.window.showErrorMessage(
+            "The variables of the nodes must be unique"
+          );
           validArgs = false;
         }
       });
       config.edges.forEach((edge: any) => {
         if (!checkUniqueness(edge!.atom.variables)) {
-          vscode.window.showErrorMessage("The variables of the edges must be unique");
+          vscode.window.showErrorMessage(
+            "The variables of the edges must be unique"
+          );
           validArgs = false;
         }
       });
     }
 
     if (validArgs) {
-
       vscode.window
         .showInputBox({
           prompt: "Enter the filename of the template file",
@@ -74,7 +73,6 @@ export function activate(context: vscode.ExtensionContext) {
           vscode.commands.executeCommand("workbench.view.explorer");
         });
     }
-
   });
 
   let exec = vscode.commands.registerCommand("asp-vis.execute", () => {
@@ -88,48 +86,6 @@ export function activate(context: vscode.ExtensionContext) {
     let img: string = process.env.IMG_DIR || "";
 
     callNode(template, program, solver, sol, out, img);
-  });
-
-  let wrapper = vscode.commands.registerCommand("asp-vis.wrapper", () => {
-    const base = path.join(__dirname, "..", "src", "sample-files");
-
-    let sol_num: number = 1;
-    let template: string = "";
-    let program: string = "";
-    let solver: string = path.join(base, "dlv2_win.exe");
-    let out_dir: string = path.join(__dirname, "..", "out");
-    let img_dir: string = path.join(base, "images");
-
-    // show an input field from vscode and check the input
-    vscode.window
-      .showInputBox({
-        prompt: "Enter the type of visualization you want to see",
-        placeHolder: "matrix, table, images, graph",
-      })
-      .then((value) => {
-        switch (value) {
-          case "matrix":
-            template = path.join(base, "matrix.json");
-            program = path.join(base, "matrix.asp");
-            break;
-          case "table":
-            template = path.join(base, "table.json");
-            program = path.join(base, "matrix.asp");
-            break;
-          case "images":
-            template = path.join(base, "images.json");
-            program = path.join(base, "images.asp");
-            break;
-          case "graph":
-            template = path.join(base, "graph.json");
-            program = path.join(base, "3col.asp");
-            break;
-          default:
-            console.log("no available visualization for this kind :(");
-            return;
-        }
-        callNode(template, program, solver, sol_num, out_dir, img_dir);
-      });
   });
 
   let convert = vscode.commands.registerCommand("asp-vis.ffmpeg", () => {
@@ -175,23 +131,7 @@ export function activate(context: vscode.ExtensionContext) {
     webview_provider
   );
 
-  const watcher = vscode.workspace.createFileSystemWatcher("**/*");
-
-  context.subscriptions.push(
-    watcher.onDidCreate((uri) => {
-      vscode.window.showInformationMessage(`File created at: ${uri.fsPath}`);
-    })
-  );
-
-  context.subscriptions.push(
-    disposable,
-    save,
-    config,
-    convert,
-    wrapper,
-    exec,
-    webview
-  );
+  context.subscriptions.push(save, config, convert, exec, webview);
 }
 
 export function deactivate() {
@@ -206,6 +146,6 @@ function checkUniqueness(args: string[]) {
     }
   }
   console.log(uniques, args);
-  
+
   return args.length === uniques.length;
 }
